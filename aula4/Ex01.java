c07c4c071bf0610811b3fbf767991f29ed50482b
andersqz <anderson.carvalho@ufn.edu.br>
2026-03-17 21:11:50 -0300

    aula 4 de poo | exercicios feitos em java e csharp

---
+++ aula4/Ex01.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        int n = input.nextInt();
+        input.nextLine();
+
+        Carro[] carro = new Carro[n];
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Marca: ");
+            String marca = input.nextLine();
+
+            System.out.print("Modelo: ");
+            String modelo = input.nextLine();
+
+            System.out.print("Ano de fabricacao: ");
+            int anoFabricacao = input.nextInt();
+            input.nextLine();
+
+            carro[i] = new Carro(marca, modelo, anoFabricacao);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Marca: " + carro[i].Marca);
+            System.out.println("Modelo: " + carro[i].Modelo);
+            System.out.println("Ano Fabricacao: " + carro[i].AnoFabricacao);
+        }
+        input.close();
+    } 
+}
+
+class Carro {
+
+    public String Marca;
+    public String Modelo;
+    public int AnoFabricacao;
+    
+    public Carro(String marca, String modelo, int anoFabricacao) {
+        Marca = marca;
+        Modelo = modelo;
+        AnoFabricacao = anoFabricacao;
+    }
+}
---
+++ poo-csharp/aula4/Ex01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Quantos objetos carros quer criar? ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Carro[] carro = new Carro[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Marca: ");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Modelo: ");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Ano de fabricacao: ");
+            int ano = int.Parse(Console.ReadLine()!);
+
+            carro[i] = new Carro(marca, modelo, ano);
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            System.Console.WriteLine("Marca: " + carro[i].Marca);
+            System.Console.WriteLine("Modelo: " + carro[i].Modelo);
+            System.Console.WriteLine("Ano: " + carro[i].AnoFabricacao);
+        }
+    }
+}
+
+class Carro
+{
+    public string Marca;
+    public string Modelo;
+    public int AnoFabricacao;
+
+    public Carro(string marca, string modelo, int anoFabricacao)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        AnoFabricacao = anoFabricacao;
+    }
+}
