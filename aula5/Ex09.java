e22c49f2dce8012b86a5d3861c1a0612166aa56e
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-03-25 23:41:09 +0000

    ex 09 de encapsulamento resolvido

--- aula5/Ex09.java
+++ aula5/Ex09.java
@@ -4,7 +4,9 @@ public class Ex09 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        Triangulo t = new Triangulo(5.0, 6.0, 4.0);
 
+        t.setLadoA(11.5);
     }
 }
 
@@ -14,5 +16,36 @@ class Triangulo {
     private double ladoB;
     private double ladoC;
 
-    public void setLadoA()
+    public Triangulo(double ladoa, double ladob, double ladoc) {
+        this.ladoA  = ladoa;
+        this.ladoB = ladob;
+        this.ladoC = ladoc;
+    }
+
+    public void setLadoA(double n) {
+        if (n > this.ladoB + this.ladoC) {
+            System.out.print("A alteração do lado A do triangulo não deve ser maior que a soma dos lados B e C");
+        }
+        else {
+            this.ladoA = n;
+        }
+    }
+
+    public void setLadoB(double n) {
+        if (n > this.ladoA + this.ladoC) {
+            System.out.print("A alteração do lado B do triangulo não deve ser maior que a soma dos lados A e C");
+        }
+        else {
+            this.ladoB = n;
+        }
+    }
+
+    public void setLadoC(double n) {
+        if (n > this.ladoB + this.ladoA) {
+            System.out.print("A alteração do lado C do triangulo não deve ser maior que a soma dos lados B e A");
+        }
+        else {
+            this.ladoC = n;
+        }
+    }
 }
