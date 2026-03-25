46bea87bd7f78468c747405fc83c96f61f88c710
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2026-03-25 19:34:26 +0000

    ex 09 incompleto

---
+++ aula5/Ex09.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex09 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
+
+
+class Triangulo {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    public void setLadoA()
+}
